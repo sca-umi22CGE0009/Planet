0280fb22c9e582bcd7b61d19363e017be40f5c39
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2024-06-08 18:43:02 +0900

    消えるブロックを修正

--- Assets/Sasaki/Scripts/BlockScript.cs
+++ Assets/Sasaki/Scripts/BlockScript.cs
@@ -7,15 +7,58 @@ using UnityEngine;
 /// </summary>
 public class BlockScript : MonoBehaviour
 {
-    [SerializeField, Header("プレイヤーが乗ってから消えるまでの時間")] private float moveTime;
+    [SerializeField, Header("プレイヤーが乗ってから消える判定をするまでの時間")] private float playerJadgmentTime = 1.0f;
+    private float timeSave;
+    [SerializeField, Header("プレイヤーが乗ってから消えるまでの時間")] private float moveTime = 0.5f;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    bool inCheck; //プレイヤーが触れているか
+
+    void Start()
+    {
+        inCheck = false;
+        timeSave = playerJadgmentTime;
+    }
+
+    void Update()
+    {
+        //プレイヤーが触れているとき
+        if (inCheck)
+        {
+            timeSave -= Time.deltaTime;
+            Debug.Log(timeSave);
+
+            //Debug.Log(timeSave);
+            //プレイヤーがplayerfadgmentTime秒乗っていたら
+            if (timeSave < 0)
+            {
+                StartCoroutine(CoolTime());
+            }
+        }
+        else
+        {
+            //初期化
+            timeSave = playerJadgmentTime;
+        }
+
+    }
+
+    //プレイヤーが触れたら
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            StartCoroutine(CoolTime());
+            inCheck = true;
         }
     }
+    //触れていないとき
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inCheck = false;
+        }
+    }
+
     //消えるまでの時間判定
     IEnumerator CoolTime()
     {
